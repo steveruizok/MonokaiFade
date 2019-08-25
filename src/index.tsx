017c7902d1a7590495f30da2f4e8b5e54d100f7b
Steve <steveruizok@gmail.com>
2019-08-25 20:15:49 +0100

    Adds import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import './fonts/Px437_IBM_VGA8.ttf'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
